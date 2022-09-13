efbde9b7a6f5a653bfd5e1a567f36ba4a0a8b7a1
yattinda <yattinda@gmail.com>
2022-09-13 12:56:02 +0900

    Done

---
+++ app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+from counter import count
+import time
+
+with st.sidebar:
+    input_date = st.date_input("when will u complete?")
+    
+#プレースホルダー
+count_area = st.empty()
+
+while(True):
+    d, h, m, s = count(input_date)
+    display = f'{d:02} days {h:02}: {m:02} : {s:02}'
+    count_area.title(display)
+    time.sleep(1)
+    

997d4b2910184abcc8a77c2a4ae118197c477124
yattinda <yattinda@gmail.com>
2022-10-28 10:26:15 +0900

    Add default time

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import streamlit as st
 from counter import count
 import time
@@ -5,12 +6,13 @@ import time
 st.title('n日後に死ぬワニ')
 
 with st.sidebar:
-    input_date = st.date_input("when is the target date?")
-    
-#プレースホルダー
+    input_date = st.date_input(
+        "when is the target date?", datetime(2023, 2, 15))
+
+# プレースホルダー
 count_area = st.empty()
 
-while(True):
+while (True):
     d, h, m, s = count(input_date)
     display = f'{d:02} days {h:02}: {m:02} : {s:02}'
     count_area.title(display)

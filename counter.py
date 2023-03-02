7737660ef70f59c31033319425f28336997e272c
yattinda <yattinda@gmail.com>
2023-03-02 22:57:54 +0900

    Omit pub_holiday

--- counter.py
+++ counter.py
@@ -1,4 +1,5 @@
 from datetime import datetime, time
+from holiday import between_holiday_num
 import workdays
 
 
@@ -7,6 +8,7 @@ def count(target_datetime):
     now_datetime = datetime.utcnow()
     remain_datetime = target_datetime - now_datetime
     d = workdays.networkdays(now_datetime, target_datetime)
+    d = d - between_holiday_num(now_datetime, target_datetime)
     m, s = divmod(remain_datetime.seconds, 60)
     h, m = divmod(m, 60)
     return d, h, m, s
